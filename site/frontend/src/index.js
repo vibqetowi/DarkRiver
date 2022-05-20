3aca2e6a20cf36379a2f805e56941d9b9208c43b
vibqetowi <abutcher.usa@protonmail.com>
2022-05-20 16:17:56 -0400

    v

--- site/frontend/src/index.js
+++ site/frontend/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HelmetProvider } from 'react-helmet-async';
-import { StoreProvider } from './Store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HelmetProvider } from "react-helmet-async";
+import { StoreProvider } from "./Store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+var PORT = process.env.PORT || 3000;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StoreProvider>
