236957b07717fe1ca564e9f4053777de29bc8cb7
vibqetowi <abutcher.usa@protonmail.com>
2022-05-22 09:51:24 -0400

    still trying to fix the damn mongodb thing

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import path from "path";
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI_LOCAL)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("connected to db");
   })
