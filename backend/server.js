a37b5581869aff85e6b02354613ec4aa7c02842b
vibqetowi <abutcher.usa@protonmail.com>
2022-05-21 23:30:31 -0400

    v

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.use("/api/upload", uploadRouter);
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
